2e525628503462038c84fc1421527f7ba7361d08
kkkont <116272343+kkkont@users.noreply.github.com>
2024-10-03 22:36:06 +0300

    Project card and list with responsive design

---
+++ src/components/projectcard/ProjectCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./ProjectCard.css";
+
+function ProjectCard({
+  title,
+  shortDescription,
+  image,
+  repositoryLink,
+  projectId,
+}) {
+  return (
+    <div className="project-card">
+      <img
+        src={process.env.PUBLIC_URL + image}
+        alt={title}
+        className="project-image"
+      />
+      <div className="project-short-info">
+        <div className="project-title-and-links">
+          <h3 className="project-title">{title}</h3>
+          <div className="project-buttons">
+            <a href={`/project/${projectId}`} className="view-details-button">
+              Read More :)
+            </a>
+          </div>
+        </div>
+        <p className="project-short-description">{shortDescription}</p>
+      </div>
+    </div>
+  );
+}
+
+ProjectCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  shortDescription: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  repositoryLink: PropTypes.string.isRequired,
+  projectId: PropTypes.number.isRequired,
+};
+
+export default ProjectCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import About from "../components/about/About";
+import ProjectCard from "../components/projectcard/ProjectCard"; // Ensure this is declared only once
+import projectsData from "../assets/data/projects.json";
 
 function Home() {
-  return <About />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects(projectsData);
+  }, []);
+
+  return (
+    <div>
+      <About />
+      <section className="projects-list">
+        <p id="projects-header">Feel free to explore my projects!</p>
+        <div className="projects-grid">
+          {projects.map((project) => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              shortDescription={project.shortDescription}
+              image={project.image}
+              repositoryLink={project.repositoryLink}
+              projectId={project.id}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Home;
