0332c208ec16a02e0d28cbfef504ce3a92f9b903
kkkont <116272343+kkkont@users.noreply.github.com>
2024-10-04 21:59:45 +0300

    Changed routing to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectPage from "./pages/ProjectPage";
 import "./App.css";
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:id" element={<ProjectPage />} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
